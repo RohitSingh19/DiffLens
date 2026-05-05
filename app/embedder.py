622cae9a5234735ae78bc2f0bb13a20972c1ed6e
Rohit Singh <Rohit.Singh@clario.com>
2026-05-05 17:46:41 +0530

    Replaced local transformer with openAI transformer

--- app/embedder.py
+++ app/embedder.py
@@ -1,18 +1,26 @@
-from sentence_transformers import SentenceTransformer
-
+from openai import OpenAI
+import os
 
 class CodeEmbedder:
     def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
     def generate_embedding(self, text: str):
         """
         Generate embedding for a single chunk
         """
-        return self.model.encode(text).tolist()
+        response = self.client.embeddings.create(
+            model="text-embedding-3-small",
+            input=text
+        )
+        return response.data[0].embedding
 
     def generate_embeddings(self, texts: list[str]):
         """
         Generate embeddings for multiple chunks
         """
-        return self.model.encode(texts).tolist()
+        response = self.client.embeddings.create(
+            model="text-embedding-3-small",
+            input=texts
+        )
+        return [item.embedding for item in response.data]
